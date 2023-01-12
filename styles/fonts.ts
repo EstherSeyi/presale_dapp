eab96ee748a771fefb3e1477e4a0d8755b19b7f1
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-01-12 14:27:04 +0100

    moves font to a different file

---
+++ styles/fonts.ts
@@ -0,0 +1,12 @@
+import { DM_Sans, Space_Grotesk } from "@next/font/google";
+
+export const dmsans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-DM-Sans",
+  weight: ["400", "500", "700"],
+});
+export const spacegrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-Space-Grotesk",
+  weight: ["700"],
+});
