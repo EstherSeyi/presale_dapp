7c60e4620dd3dec6cc5319911a0251b05ac1cbcb
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-01-12 21:35:47 +0100

    updates index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import dynamic from "next/dynamic";
+
+import BuyForm from "../src/components/BuyForm";
+
+const ViewBalance = dynamic(() => import("../src/components/ViewBalance"), {
+  ssr: false,
+});
+const timeline = [
+  "Sale Pending",
+  "Sale Live",
+  "Sale Completed",
+  "Vesting Started",
+  "EXX Launched",
+];
+
 export default function Home() {
   return (
     <section className="flex flex-col md:flex-row  mt-20">
@@ -10,20 +25,22 @@ export default function Home() {
           </p>
         </div>
         <ul className="hidden md:block">
-          <li>Sale Pending</li>
-          <li>Sale Live</li>
-          <li>Sale Completed</li>
-          <li>Vesting Started</li>
-          <li>EXX Launched</li>
+          {timeline.map((label, index) => {
+            return (
+              <li key={label} className="stepper-row">
+                <div className="dot-container">
+                  <div className="dot"></div>
+                  {index === 0 && <div className="shade"></div>}
+                </div>
+                <p>{label}</p>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="md:basis-5/12 md:ml-4">
-        <div className="hidden md:block bg-offwhite py-3 px-4 max-w-fit rounded-lg mb-4">
-          <span className="text-xs text-blue_01 font-bold">
-            Your bal: 0.0000034 USDT
-          </span>
-        </div>
-        <div className="bg-white rounded-xl p-5">
+        <ViewBalance />
+        <div className="bg-white rounded-xl p-5 mb-4 md:mb-0">
           <div className="flex justify-between mb-9">
             <span className="font-spacegrotesk text-lg">10:20:30</span>
             <button className="bg-yellow text-xs px-4 py-2 rounded-2xl font-bold">
@@ -37,35 +54,8 @@ export default function Home() {
               <div className="bg-blue_01 h-2.5 rounded-full w-[45%]"></div>
             </div>
           </div>
-          <form>
-            <label className="font-spacegrotesk ">Enter Amount to buy</label>
-            <div className="mt-2">
-              <div className="flex justify-between">
-                <input
-                  placeholder="00.00 USDT"
-                  className="border border-blue_02 rounded-lg py-2 pl-1.5 text-black mr-3 flex-grow"
-                />
-                <button className="bg-blue_01 text-white py-2 px-5 rounded-lg font-bold">
-                  Buy
-                </button>
-              </div>
-            </div>
-            <p className="text-sm mb-8 mt-1">
-              <span className="text-grey_03 mr-4">
-                Min. Buy:{" "}
-                <span className="text-black font-medium font-dmsans">
-                  30 USDT
-                </span>
-              </span>
 
-              <span className="text-grey_03">
-                Max. Buy:{" "}
-                <span className="text-black font-medium font-dmsans">
-                  10,000 USDT
-                </span>
-              </span>
-            </p>
-          </form>
+          <BuyForm />
 
           <div>
             <h2 className="font-bold mb-2">Disclaimer</h2>
